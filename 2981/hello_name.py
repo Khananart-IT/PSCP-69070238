aad48b4ee830ae47f0931e95aef987a13b17d39b
nonlovesmom <69070238@kmitl.ac.th>
2026-07-03 12:20:51 +0700

    hello_name

---
+++ 2981/hello_name.py
@@ -0,0 +1,7 @@
+'''Variables'''
+NAME = input("Please enter your name: ")
+SURNAME = input("Please enter your surname: ")
+
+'''Output'''
+print(f"Hello {NAME} {SURNAME}")
+print(f"{NAME[:2]}{SURNAME[:2]}")
